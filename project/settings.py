e8eff154174d09ded042e81c3532e893bc6635db
Cristian David <usuario1@LO-ADM-04.flowers.local>
2025-06-14 12:15:57 -0500

    frontend project

--- project/settings.py
+++ project/settings.py
@@ -92,6 +92,13 @@ TIME_FORMAT = 'H:i'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
